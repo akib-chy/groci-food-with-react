ab6b89b8e8647dddccac031884b141e5246a50df
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-29 22:48:33 +0600

    service section add

--- src/Page/OurService/OurService.jsx
+++ src/Page/OurService/OurService.jsx
@@ -1,64 +1,35 @@
 import React from "react";
-
+import "./OurService.css";
 const OurService = () => {
   return (
-    <div className="container">
-      <div className="text-center">
-        <small>We Provide the Best</small>
-        <h2>Our Service</h2>
-      </div>
-      <div>
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-            </div>
+    <div className="container service my-5">
+      <h2 className="text-center mb-4 text-warning fw-bold">Our Service</h2>
+      <div className="row">
+        <div className="col-lg-4 col-md-6 col-sm-12 d-flex align-items-center">
+          <div className="service-icon">
+            <i className="fa-solid fa-truck-fast"></i>
           </div>
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-            </div>
+          <div>
+            <h5 className="m-0">Free & Next Day Delivery</h5>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
           </div>
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content.
-                </p>
-              </div>
-            </div>
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12 d-flex align-items-center">
+          <div className="service-icon">
+            <i className="fa-solid fa-basket-shopping"></i>
           </div>
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-            </div>
+          <div>
+            <h5 className="m-0">100% Satisfaction Guarantee</h5>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12 d-flex align-items-center">
+          <div className="service-icon">
+            <i className="fa-solid fa-tags"></i>
+          </div>
+          <div>
+            <h5 className="m-0">Great Daily Deals Discount</h5>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
           </div>
         </div>
       </div>
